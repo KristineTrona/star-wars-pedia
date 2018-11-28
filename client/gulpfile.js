4e01164549221c4f847403b9bb948de21f46f67f
Kristine <kristinetrona@gmail.com>
2018-11-28 13:42:03 +0100

    initial setup complete

---
+++ client/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  return gulp.src('./src/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/'));
+});
+
+gulp.task('bootstrap', function(){
+  return gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch(['./src/*.scss'], ['sass']); 
+  gulp.watch(['./node_modules/bootstrap/scss/bootstrap.scss'], ['bootstrap']); 
+})
